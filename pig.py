672a974c9bf66d25510e995850f9565b48b13146
casper-hansen <60180409+casperHansenTheEnemyOfAustria@users.noreply.github.com>
2022-08-29 10:38:04 +0200

    Startup

---
+++ pig.py
@@ -0,0 +1,19 @@
+import random as r
+
+def singe_game(player_name):
+    user_input = input()
+    total_score = 0 
+    dice = 0
+    while user_input == "yes" or user_input == "y" and dice != 1 :
+        print("rolling")
+        dice = r.randrange(1,7)
+        if dice == 1:
+            total_score = 0 
+            break
+        else:
+            total_score += dice
+        print("total score is {}".format(total_score))
+
+        print("Keep going? y, n")
+        user_input = input()
+    
