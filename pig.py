7eee49e6fa4622a304298b42882fa293d94e60cb
casper-hansen <60180409+casperHansenTheEnemyOfAustria@users.noreply.github.com>
2022-08-31 10:01:05 +0200

    small updates to formatting

--- pig.py
+++ pig.py
@@ -1,21 +1,26 @@
 import random as r
 
+    
+
 def single_game(player_name):
     print("Hello! "+player_name+" do you want to start your round?")
     user_input = input()
-    total_score = 0 
+    round_score = 0 
     dice = 0
     while user_input == "yes" or user_input == "y" and dice != 1:
         print("rolling")
         dice = r.randrange(1,7)
         if dice == 1:
-            total_score = 0
+            round_score = 0
             print("oops, next player")
             break
         else:
-            total_score += dice
-        print(f"total score is {total_score}")
+            round_score += dice
+        print(f"total score is {round_score}")
 
         print("Keep going? y, n")
         user_input = input()
+    return round_score
 # single_game("Casper")
+
+# player.total_score += single_game(player.name)
